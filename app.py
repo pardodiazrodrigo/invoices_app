674dd3d44de535335d131d790005759ae5ec9ea9
pardodiazrodrigo <pardo.diaz.rodrigo@gmail.com>
2024-02-22 16:47:17 -0300

    Remove name main start

--- app.py
+++ app.py
@@ -13,9 +13,3 @@ app.include_router(users.router)
 @app.get("/",  tags=["Root"])
 async def root():
     return {"message": "Server is running"}
-
-
-if __name__ == "__main__":
-    import uvicorn
-
-    uvicorn.run(app, host="127.0.0.1", port=8000)
