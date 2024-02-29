8398624f496f3ae6f55d943e6740eeb00f921aa2
pardodiazrodrigo <pardo.diaz.rodrigo@gmail.com>
2024-02-29 13:01:37 -0300

    tests for invoices

--- routers/invoices.py
+++ routers/invoices.py
@@ -34,7 +34,7 @@ async def create_invoice(user: user_dependency, invoice: InvoiceCreate, db: Sess
     return Invoice(**db_invoice.__dict__)
 
 
-@router.get("/invoice/{invoice_id}", status_code=status.HTTP_200_OK)
+@router.get("/invoice", status_code=status.HTTP_200_OK)
 async def read_invoice(user: user_dependency, invoice_id: int, db: Session = Depends(get_db)):
     try:
         db_invoice = read_db_invoice(invoice_id, db, user.get('id'))
--- test/test_invoice.py
+++ test/test_invoice.py
@@ -29,8 +29,8 @@ def override_get_current_user():
     """
     Overrides the get_current_user function to return a testing user
     """
-    username = 'admin'
-    role = 'admin'
+    username = 'user'
+    role = 'user'
     user_id = 1
     return {"username": username, "id": user_id, 'role': role}
 
@@ -45,12 +45,36 @@ client = TestClient(app)
 def test_invoice():
     invoice = InvoiceDB(
         invoice_number="0002-000722",
-        date= "2024-02-27",
+        date="2024-02-27",
         cuit="20345678999",
         client="Test A S.A.",
         amount=1122.50,
         paid=False,
         user_id=1,
+    )
+
+    db = TestingSessionLocal()
+    db.add(invoice)
+    db.commit()
+    db.refresh(invoice)
+
+    yield invoice
+
+    with engine.connect() as conn:
+        conn.execute(text("DELETE FROM invoice;"))
+        conn.commit()
+
+
+@pytest.fixture
+def test_invoice_user_id_2():
+    invoice = InvoiceDB(
+        invoice_number="0002-000722",
+        date="2024-02-27",
+        cuit="20345678999",
+        client="Test A S.A.",
+        amount=1122.50,
+        paid=False,
+        user_id=2,
     )
 
     db = TestingSessionLocal()
@@ -79,8 +103,8 @@ def test_read_all_authenticated(test_invoice):
 
 
 def test_read_invoice_authenticated(test_invoice):
-    response = client.get("/api/invoice?id=1")
-    assert response.status_code == status.HTTP_200_OK, "No dio 200?"
+    response = client.get("/api/invoice?invoice_id=1")
+    assert response.status_code == status.HTTP_200_OK
     assert response.json() == {'client': 'Test A S.A.',
                                'invoice_number': '0002-000722',
                                'date': '2024-02-27',
@@ -89,3 +113,96 @@ def test_read_invoice_authenticated(test_invoice):
                                'cuit': '20345678999',
                                'amount': 1122.5,
                                'paid': False}
+
+
+def test_read_invoice_unauthenticated_not_found(test_invoice):
+    response = client.get("/api/invoice?invoice_id=999")
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    assert response.json() == {'detail': "Not Found"}
+
+
+def test_read_invoice_authenticated_user_id(test_invoice_user_id_2):
+    response = client.get("/api/invoice?invoice_id=2")
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    assert response.json() == {
+        "detail": "Not Found"
+    }
+
+
+def test_read_all_invoices(test_invoice):
+    response = client.get("/admin/invoices")
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    assert response.json() == {
+        "detail": "Unauthorized"
+    }
+
+
+def test_create_invoice(test_invoice):
+    request_data = {
+        "invoice_number": "0011-000722",
+        "date": "2024-02-27",
+        "cuit": "20345678999",
+        "client": "Test A S.A.",
+        "amount": 1122.50,
+        "paid": False,
+        "user_id": 10
+    }
+    response = client.post("/api/invoice", json=request_data)
+    db = TestingSessionLocal()
+    invoice = db.query(InvoiceDB).filter(InvoiceDB.id == 2).first()
+    assert response.status_code == status.HTTP_201_CREATED
+    assert invoice.invoice_number == "0011-000722"
+    assert invoice.client == "Test A S.A."
+
+
+def test_update_invoice(test_invoice):
+    request_data = {
+        "invoice_number": "0011-000722",
+        "date": "2024-02-27",
+        "cuit": "20345678999",
+        "client": "Test A S.A.",
+        "amount": 1122.50,
+        "paid": True,
+        "id": 1
+    }
+    respone = client.put("/api/invoice", json=request_data)
+    db = TestingSessionLocal()
+    invoice = db.query(InvoiceDB).filter(InvoiceDB.id == 1).first()
+    assert respone.status_code == status.HTTP_204_NO_CONTENT
+    assert invoice.invoice_number == "0011-000722"
+    assert invoice.paid is True
+
+
+def test_update_invoice_not_found(test_invoice):
+    request_data = {
+        "invoice_number": "0011-000722",
+        "date": "2024-02-27",
+        "cuit": "20345678999",
+        "client": "Test A S.A.",
+        "amount": 1122.50,
+        "paid": True,
+        "id": 999
+    }
+    response = client.put("/api/invoice", json=request_data)
+    db = TestingSessionLocal()
+    invoice = db.query(InvoiceDB).filter(InvoiceDB.id == 999).first()
+    assert invoice is None
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    assert response.json() == {'detail':  'Not Found'}
+
+
+def test_delete_invoice(test_invoice):
+    response = client.delete("/api/invoice?invoice_id=1")
+    db = TestingSessionLocal()
+    invoice = db.query(InvoiceDB).filter(InvoiceDB.id == 1).first()
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+    assert invoice is None
+
+
+def test_delete_invoice_not_found(test_invoice):
+    response = client.delete("/api/invoice?invoice_id=999")
+    db = TestingSessionLocal()
+    invoice = db.query(InvoiceDB).filter(InvoiceDB.id == 999).first()
+    assert invoice is None
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    assert response.json() == {'detail': 'Not Found'}
